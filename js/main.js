e2c65aa0b464881fc503bd5be35fc1001201312c
Carmina <cvdsoto@gmail.com>
2020-05-18 11:51:58 +1000

    cleaned up js file and added comments

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
+//get all nav links except contact icons
 const nav = document.querySelectorAll('a.nav-link');
 
+//scroll down to each corresponding ID smoothly
 nav.forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -10,42 +12,41 @@ nav.forEach(anchor => {
     });
 });
 
-mybutton = document.getElementById("toTop");
-// console.log('button', mybutton);
-
-// When the user scrolls down 20px from the top of the document, show the button
+//get back to top button and main element (this is where the scrollbar is)
+myButton = document.getElementById("toTop");
 main = document.getElementById("main");
 
+//when the user scrolls down 20px from the top of the main tag, show the button
 main.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
 
   if (main.scrollTop > 20) {
-    mybutton.style.display = "block";
+    myButton.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    myButton.style.display = "none";
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+//when the user clicks on the button, scroll to the top which is the home id
 function topFunction() {
   document.getElementById("home").scrollIntoView({
     behavior: 'smooth'
   })
 }
 
-const topButton = function(w_width, m_width, w_height, m_height) {
+//calculate position of the back to top button using window width, main width, window height and document height
+const topButton = function(w_width, m_width, w_height, d_height) {
   const width = ((w_width - m_width) / 2) + 20;
-  const length = (w_height - m_height) + 80;
-  mybutton.style.right = width.toString() + 'px';
-  mybutton.style.bottom = length.toString() + 'px';
+  const length = (w_height - d_height) + 80;
+  myButton.style.right = width.toString() + 'px';
+  myButton.style.bottom = length.toString() + 'px';
 }
 
+//initial position of the button based on window size
 topButton(window.innerWidth, main.clientWidth, window.innerHeight, document.body.clientHeight);
 
+//dynamic positioning of the button when the window is resized
 window.addEventListener('resize', function(event){
-  console.log('window height', window.innerHeight);
-  console.log('document length', document.body.clientHeight);
-  console.log('main length:', main.clientHeight);
   topButton(window.innerWidth, main.clientWidth, window.innerHeight, document.body.clientHeight);
 })
